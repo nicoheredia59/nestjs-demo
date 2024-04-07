649eb9f2091b1ccf63e99a5db1e2f9daddcd3f78
nicoheredia59 <nicoheredia59@gmail.com>
2024-04-07 11:46:31 -0300

    feat: app module

---
+++ src/app.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
+import { DatabaseModule } from './database/database.module';
+import { UserModule } from './user/user.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    DatabaseModule,
+    UserModule,
+  ],
+})
+export class AppModule {}
